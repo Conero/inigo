ce9677b571652cfa73c63d4e79a8fd5296ed3a96
Joshua Conero <brximl@163.com>
2018-08-19 10:13:30 +0800

    init the documnet branch

--- parse.go
+++ parse.go
@@ -1,158 +1,376 @@
-/* @ini-go V1.x
-* @Joshua Conero
-* @2017年10月28日 星期六
-* @ini 文件解释器重写
+/* @Joshua Conero
+ * @2017年1月19日 星期四
+ * @ini 文件解释器
  */
 
-package inigo
+package ini
 
 import (
-	"strconv"
-	"strings"
+	"bufio"
+	"io/ioutil"
+	"os"
+	"os/user"
+	"path"
 	"regexp"
+	"strings"
+	"time"
 )
 
-// ini 结构体
-type Ini struct {
-	Liner                            // 组合继承- 行处理
-	FileName  string                 // ini 文件
-	DataQueue map[string]interface{} // ini 解析后的数据
-	IsSuccess bool                   // ini 文件是否解析成功
-	FailMsg   string                 // 错误信息
-	File      *File                  // 文件解析信息
+// 解析以后的数据
+var paserData map[string]interface{}
+
+// 当前计算机用户信息
+type baseUser struct {
+	__DIR__  string // 当前目录
+	__USER__ string // 当前用户
+	__GID__  string
+	__NAME__ string
+	__UID__  string
 }
 
-// 入口文件
-func Open(name string) *Ini {
-	// 初始化对象
-	inier := &Ini{
-		FileName:  name,
-		DataQueue: map[string]interface{}{},
-		IsSuccess: false,
-		FailMsg:   "",
-		File: &File{
-			line: 0,
-		},
+// 编译器属性
+type Compiler struct {
+	filename  string    // 文件名称
+	content   string    // 内容
+	mtime     time.Time // 用于计算时间差-效率评估量
+	endtime   time.Time // 完成时间
+	seekCount int       // 遍历次数
+	lineCount int       // 行数
+}
+
+// 压缩器属性
+type Compresser struct {
+	filename  string    // 文件名称
+	content   string    // 内容
+	mtime     time.Time // 用于计算时间差-效率评估量
+	endtime   time.Time // 完成时间
+	seekCount int       // 遍历次数
+	lineCount int       // 行数
+}
+
+var currentConf *Conf            // 当前的文件
+var currentCompiler Compiler     // 当前编译器对象
+var currentCompresser Compresser // 当前压缩器对象
+
+// 当前用户
+var currentUser baseUser
+
+// 构造函数-打开配置文件
+func Open(fname string) *Conf {
+	currentConf = &Conf{
+		filename: fname,
+		content:  "",
+		mtime:    time.Now(),
 	}
-	inier.reader()
-	return inier
+	return currentConf
 }
 
-// 私有方法，文件读取
+// 构造函数-设置字符串
+func Content(vtext string) *Conf {
+	currentConf = &Conf{
+		filename: "",
+		content:  vtext,
+		mtime:    time.Now(),
+	}
+	return currentConf
+}
 
-func (I *Ini) reader() {
-	if len(I.FileName) > 0 {
-		ln := NewLnRer(I.FileName)
-		// 行扫描
-		bba := BBAnalyze(I)
-		I.IsSuccess = ln.Scan(func(line string) {
-			line = strings.TrimSpace(line)
-			I.File.countLine()
-			// 非错误
-			//if !isPanicError {
-			// 空行
-			if len(line) == 0 {
-				return
-			}
-			// 多行注释
-			if I.isMultiComment(line) {
-				return
-			}
-			// 单行注释
-			if I.isComment(line) {
-				return
-			}
-			// 键值结束
-			if line == IniParseSettings["scope2"] {
-				bba.CommitQueue()
-				return
-			}
-			line = I.transComment(line, false)
-			regCmt := regexp.MustCompile(IniParseSettings["reg_has_comment"])
-			CmtIdx := regCmt.FindStringIndex(line)
-			if CmtIdx != nil{
-				line = strings.TrimSpace(line[0:CmtIdx[0]])
-			}
-			line = I.transComment(line, true)
-			// 多行字符串/字符串数组
-			isMl, isEnd, mKey, mValue := I.mLineString(line)
-			if isMl {
-				return
-			}
-			// 多行数组结束
-			if isEnd {
-				if len(mKey) > 0{
-					bba.PushQueue(mKey, mValue)
-				}else{
-					bba.MultiLineToArray(mValue)
+// 解析文本
+func (c *Conf) Parse() (int, map[string]interface{}) {
+	var status int = 0
+	paserData = make(map[string]interface{})
+	seekCount := 0
+	lineCount := 0
+	if c.filename != "" {
+		c.mtime = time.Now()
+		// 逐行读取文件
+		fh, err := os.Open(c.filename)
+		multiLine := false // 多行注释开启
+		if err == nil {
+			buf := bufio.NewReader(fh)
+			for {
+				line, err := buf.ReadString('\n')
+				// 程序跳转前检测是否出错，出错直接中断循环，避免还没有检查错误时便继续进入循环(死循环)
+				if err != nil {
+					break
 				}
-				return
+				line = strings.TrimSpace(line)
+				lineCount = lineCount + 1
+				// 多行注释开始
+				if line == ">>>" {
+					multiLine = true
+				} else if line == "<<<" { // 注释结束
+					multiLine = false
+					continue
+				}
+				// 循环跳出
+				if line == "" || strings.Index(line, "#") == 0 || multiLine { // # 字符换跳过
+					continue
+				}
+				key := ""
+				// 模式一 key = v1 , v2 , v3	  ; key = v
+				eqSigner := regexp.MustCompile(`=+`)
+				if eqSigner.Match([]byte(line)) {
+					// 删除行之间的所有空格
+					kbSinger := regexp.MustCompile(`\s`)
+					line = string(kbSinger.ReplaceAll([]byte(line), []byte("")))
+					strLen := strings.Index(line, "=")
+					key = line[0:strLen]
+					line = strings.TrimSpace(line[strLen+1:])
+					line = Render(line)
+					if strings.Index(line, ",") > -1 {
+						paserData[key] = strings.Split(line, ",")
+					} else {
+						paserData[key] = line
+					}
+				} else {
+					// 模式二 key v1 v2 v3; key v
+					strLen := strings.Index(line, " ")
+					key = line[0:strLen]
+					line = strings.TrimSpace(line[strLen+1:])
+					line = Render(line)
+					// 删除多空格
+					mkbSinger := regexp.MustCompile(`[\s]{2:}`)
+					if mkbSinger.Match([]byte(line)) {
+						line = string(mkbSinger.ReplaceAll([]byte(line), []byte("")))
+					}
+					if strings.Index(line, " ") > -1 {
+						paserData[key] = strings.Split(line, " ")
+					} else {
+						paserData[key] = line
+					}
+				}
+				seekCount = seekCount + 1
 			}
-
-			// 获取基键
-			isBK, BK, nLine := I.getBaseKey(line)
-			//fmt.Println(isBK, BK, nLine, line)
-			if isBK { // 是基键
-				bba.UpdateBaseKey(BK)
-				return
-			} else if BK != "" {
-				//bba.PushQueue(BK, nLine)
-				bba.PushQueue(BK, I.strToData(nLine))
-			} else {
-				bba.MultiLineToArray(nLine)
-			}
-		})
-		if !I.IsSuccess{
-			I.FailMsg = ln.Error()
+			c.seekCount = seekCount
+			c.lineCount = lineCount
+			c.endtime = time.Now()
 		}
-		I.DataQueue = bba.DataQueue
+		status = 1
 	}
+	return status, paserData
 }
 
-// 读取值
-// 支持点级多级数据查询
-func (I *Ini) Get(key string) (bool, interface{}) {
-	//value, has := I.DataQueue[key]
-	has := false
-	var value interface{}
-	// 默认数据类型
-	value = I.DataQueue
-	for _,v := range strings.Split(key, "."){
-		switch value.(type) {
-		case map[string]interface{}:
-			tValue, tHas := value.(map[string]interface{})[v]
-			has = tHas
-			if has{
-				value = tValue
-			}
-		}
-	}
-	return has, value
+// 设置编译文件名称
+func (c *Conf) SetCompileFname(fname string) *Conf {
+	currentCompiler.filename = fname
+	return c
 }
 
-// 读取函数为字符串
-// 支持点操作，多级数据获取
-func (I *Ini) GetString(key string) string {
+// 编译ini文件
+func (c *Conf) Compile() (bool, string) {
+	fname := c.filename
+	if len(fname) > 0 {
+		currentCompiler.mtime = time.Now()
+		contentBytes, err := ioutil.ReadFile(fname)
+		// 文件不存在
+		if err != nil {
+			return false, fname + "文件读取失败"
+		}
+		content := string(contentBytes)
+		content = Render(content)
+		newFileName := currentCompiler.filename
+		if len(newFileName) < 1 {
+			ext := path.Ext(fname)
+			newFileName = strings.Replace(fname, ext, ".compile"+ext, -1)
+		}
+		fname = newFileName
+		// 文件写入
+		isSuccess := ioutil.WriteFile(newFileName, []byte(content), 0x644)
+		if isSuccess != nil {
+			return false, fname + "文件写入失败"
+		}
+		currentCompiler.content = content
+		currentCompiler.endtime = time.Now()
+		return true, fname
+	}
+	return false, fname
+}
+
+// 设置编译文件名称
+func (c *Conf) SetCompressFname(fname string) *Conf {
+	currentCompresser.filename = fname
+	return c
+}
+
+// 压缩ini文件
+func (c *Conf) Compress() (bool, string) {
+	fname := c.filename
+	if len(fname) > 0 {
+		// 逐行读取文件
+		fh, err := os.Open(fname)
+		multiLine := false // 多行注释开启
+		content := ""
+		if err == nil {
+			buf := bufio.NewReader(fh)
+			for {
+				line, err := buf.ReadString('\n')
+				line = strings.TrimSpace(line)
+
+				// 多行注释开始
+				if line == ">>>" {
+					multiLine = true
+				} else if line == "<<<" { // 注释结束
+					multiLine = false
+					continue
+				}
+				// 循环跳出
+				if line == "" || strings.Index(line, "#") == 0 || multiLine { // # 字符换跳过
+					continue
+				}
+				content += line
+				if err != nil {
+					break
+				}
+			}
+			newFileName := currentCompresser.filename
+			if len(newFileName) < 1 {
+				ext := path.Ext(fname)
+				newFileName = strings.Replace(fname, ext, ".min"+ext, -1)
+			}
+			fname = newFileName
+			// 文件写入
+			isSuccess := ioutil.WriteFile(newFileName, []byte(content), 0x644)
+			if isSuccess != nil {
+				return false, fname + "文件写入失败"
+			}
+			currentCompresser.content = content
+			currentCompresser.endtime = time.Now()
+			return true, fname
+
+		}
+	}
+	return false, fname + "文件读取失败"
+}
+
+// 获取遍历次数
+func (c *Conf) GetseekCount() int {
+	return c.seekCount
+}
+
+// 获取编译次数
+func (c *Conf) GetlineCount() int {
+	return c.lineCount
+}
+
+// 键值获取
+func (c *Conf) Get(key string) (interface{}, bool) {
+	value, has := paserData[key]
+	return value, has
+}
+
+// interface{} 转 -> string
+func (c *Conf) GetString(key string) string {
+	v, exist := c.Get(key)
 	value := ""
-	exist, anyType := I.Get(key)
 	if exist {
-		switch anyType.(type) {
+		switch v.(type) {
 		case string:
-			value = anyType.(string)
-		case int:
-			value = strconv.Itoa(anyType.(int))
+			value = v.(string)
+		case []string:
+			value = strings.Join(v.([]string), ",")
 		}
 	}
+
 	return value
 }
 
-// 是否存在值
-func (I *Ini) HasKey(key string) bool {
-	_, exist := I.DataQueue[key]
-	return exist
+// interface{} 转 -> Array
+//func (c *Conf) GetArray(key string) array[]string {}
+func (c *Conf) GetArray(key string) []string {
+	var tmpArray []string
+	v, exist := c.Get(key)
+	if exist {
+		switch Arr := v.(type) {
+		case string:
+			tmpArray = append(tmpArray, v.(string))
+		case []string:
+			tmpArray = v.([]string)
+		default:
+			out(Arr)
+
+		}
+	}
+	return tmpArray
 }
 
-// 转化为json字符串
-func (I *Ini) ToJsonString() string {
-	return MkCreator(I.DataQueue).ToJsonString()
+// 系统常量转换 -> {变量名}
+func Render(linStr string) string {
+	keysMatch := regexp.MustCompile(`\{[\$a-zA-Z\d_-]+\}`)
+	keys := keysMatch.FindAllStringSubmatch(linStr, -1)
+	name := ""
+	tplKey := ""
+	signerMatch := regexp.MustCompile(`\{|\}`)
+	for _, v := range keys {
+		if len(v) > 0 {
+			value := ""
+			name = v[0]
+			tplKey = string(signerMatch.ReplaceAll([]byte(name), []byte("")))
+			switch tplKey {
+			case "$dir":
+				value = "."
+			case "$parentDir":
+				value = ".."
+			case "__DIR__":
+				value = GetBaseDir()
+			case "__USER__":
+				if len(currentUser.__USER__) < 1 {
+					GetBaseDir()
+					value = currentUser.__USER__
+				} else {
+					value = currentUser.__USER__
+				}
+			case "__GID__":
+				if len(currentUser.__GID__) < 1 {
+					GetBaseDir()
+					value = currentUser.__GID__
+				} else {
+					value = currentUser.__GID__
+				}
+			case "__NAME__":
+				if len(currentUser.__NAME__) < 1 {
+					GetBaseDir()
+					value = currentUser.__NAME__
+				} else {
+					value = currentUser.__NAME__
+				}
+			case "__UID__":
+				if len(currentUser.__UID__) < 1 {
+					GetBaseDir()
+					value = currentUser.__UID__
+				} else {
+					value = currentUser.__UID__
+				}
+			default:
+				pv, has := paserData[tplKey]
+				if has {
+					switch pv.(type) {
+					case string:
+						value = pv.(string)
+					}
+				}
+			}
+			linStr = strings.Replace(linStr, name, value, -1)
+		}
+	}
+	return linStr
+}
+
+// 获取用于当前路径地址
+func GetBaseDir() string {
+	if len(currentUser.__DIR__) > 0 {
+		return currentUser.__DIR__
+	} else {
+		user, err := user.Current()
+		if err == nil {
+			currentUser = baseUser{
+				__DIR__:  user.HomeDir,
+				__USER__: user.Username,
+				__GID__:  user.Gid,
+				__NAME__: user.Name,
+				__UID__:  user.Uid,
+			}
+		}
+	}
+	return currentUser.__DIR__
 }
