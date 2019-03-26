61d057488cdc02fa7881f7a57ac5d1a910830264
Conero <conero@163.com>
2019-03-26 15:50:59 +0800

    v2.0 版本测试

--- bin/v2/v2.0.go
+++ bin/v2/v2.0.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"fmt"
-	"inigo"
+	"github.com/conero/inigo"
+	"time"
 )
 
 type Cs struct {
@@ -12,7 +13,8 @@ func (c Cs) NewBase() {
 	ini := inigo.NewParser()
 	ini.OpenFile("./base.ini")
 
-	//fmt.Println(ini.GetData())
+	fmt.Println(ini.IsValid())
+	fmt.Println(ini.GetData())
 
 	fmt.Println(ini.Section("data", "name"))
 }
@@ -22,8 +24,18 @@ func (c Cs) newRong() {
 	rong.OpenFile("./rong.ini")
 	fmt.Println(rong.GetData())
 }
+
+func (c Cs) createBase()  {
+	base := inigo.NewParser()
+	base.Set("name", time.Now().String())
+	fmt.Println(base.Driver())
+	fmt.Println(base.GetData())
+	base.SaveAsFile("./createBase.ini")
+}
+
 func main() {
 	var cs Cs
-	cs.NewBase()
+	//cs.NewBase()
 	//cs.newRong()
+	cs.createBase()
 }
